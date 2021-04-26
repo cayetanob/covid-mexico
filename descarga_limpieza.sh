6a6037f3e82740f06c0aad3a95b0d3d455038af6
Cayetano <cayetano@gmail.com>
2021-04-26 13:06:40 -0700

    Add files via upload

--- descarga_limpieza.sh
+++ descarga_limpieza.sh
@@ -3,6 +3,6 @@ curl -O http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abier
 
 unzip  datos_abiertos_covid19.zip
 
-csvcut  -c ENTIDAD_UM,SEXO,FECHA_DEF,EDAD,CLASIFICACION_FINAL  COVID19MEXICO.csv  | csvgrep -c EMBARAZO -m "1"|  csvgrep -c ENTIDAD_UM -m "26"| csvgrep -c SEXO -m "1"| csvgrep -c CLASIFICACION_FINAL -m "1" > bd_reducida.csv
+csvcut  -c ENTIDAD_UM,SEXO,FECHA_DEF,EDAD,CLASIFICACION_FINAL  COVID19MEXICO.csv  | csvgrep -c JOV -m "1"|  csvgrep -c ENTIDAD_UM -m "26"| csvgrep -c SEXO -m "1"| csvgrep -c CLASIFICACION_FINAL -m "1" > bd_reducida.csv
 
 
