869167a8efff7376e6453856400d832c52c861de
Cayetano <cayetano@gmail.com>
2021-04-26 19:16:18 -0700

    Update descarga_limpieza.sh

--- descarga_limpieza.sh
+++ descarga_limpieza.sh
@@ -4,10 +4,15 @@ echo "Iniciando descarga de archivos - Puede tardar unos minutos "
 
 curl -O http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip
 
+#Ya descompreso el archivo es mayor a 1GB
 unzip  datos_abiertos_covid19.zip
 
 # Creando el archivo de datos final csv.
-csvcut  -c ENTIDAD_UM,SEXO,FECHA_DEF,EDAD,CLASIFICACION_FINAL  COVID19MEXICO.csv  | csvgrep -c JOV -m "1"|  csvgrep -c ENTIDAD_UM -m "26"| csvgrep -c SEXO -m "1"| csvgrep -c CLASIFICACION_FINAL -m "1" > bd_reducida.csv
+fecha=$(date -u +"%y%m%d")   # genera año mes dia 
+csvgrep -c MUNICIPIO_RES -m "030" $fecha"COVID19MEXICO.csv" > CovidHermosillo.csv
+
+#Nos presenta la fecha con el mayor número de casos en Hermosillo. 
+csvstat -c 13 CovidHermosillo.csv > FechaMayorHillo.csv
 
 rm datos_abiertos_covid19.zip
 
