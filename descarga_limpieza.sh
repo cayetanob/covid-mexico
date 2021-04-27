132ac8a34d65616215cef5bf34ce10917f86dd5c
Cayetano <cayetano@gmail.com>
2021-04-26 17:02:08 -0700

    Update descarga_limpieza.sh

--- descarga_limpieza.sh
+++ descarga_limpieza.sh
@@ -1,12 +1,15 @@
-echo "Iniciando descarga de archivos "
+#!/bin/bash
+
+echo "Iniciando descarga de archivos - Puede tardar unos minutos "
 
 curl -O http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip
 
 unzip  datos_abiertos_covid19.zip
 
+# Creando el archivo de datos final csv.
 csvcut  -c ENTIDAD_UM,SEXO,FECHA_DEF,EDAD,CLASIFICACION_FINAL  COVID19MEXICO.csv  | csvgrep -c JOV -m "1"|  csvgrep -c ENTIDAD_UM -m "26"| csvgrep -c SEXO -m "1"| csvgrep -c CLASIFICACION_FINAL -m "1" > bd_reducida.csv
 
-rm -r datos_abiertos_covid19.zip
+rm datos_abiertos_covid19.zip
 
 echo "Descarga y preparación de archivos finalizada"
 
